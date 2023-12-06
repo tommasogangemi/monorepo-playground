d42abd4593dfd33c18ca97115302aeae4816afab
tommasogangemi <tommaso.gangemi@kampaay.com>
2023-12-06 14:23:32 +0100

    feat(deps): added example of deps not being recognized across

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -3,9 +3,14 @@
 </template>
 
 <script setup lang="ts">
+// @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
+
 defineComponent({ name: 'NuxtProjectPage' })
 
 type WithName = { name: string }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const a: WithName = { name: '22' }
 </script>
--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+// @ts-expect-error
+import { ref } from 'vue'
 
 type WithName = { name: string }
 
